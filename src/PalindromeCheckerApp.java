ad9fded39ace0f8a9cae501cde0677a8ecf05ad3
himanish-srivastava <himanishsri624@gmail.com>
2026-03-06 15:32:29 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,20 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
-        // Hardcoded string
+        // Original string
         String word = "madam";
+        // Variable to store reversed string
+        String reversed = "";
 
-        // Reverse the string
-        String reversed = new StringBuilder(word).reverse().toString();
-
-        // Check if palindrome
-        if(word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome");
+        // Reverse the string using a loop
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
         }
-        else {
+
+        // Compare original and reversed string using .equals()
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
             System.out.println(word + " is NOT a Palindrome");
         }
-
     }
-
 }
